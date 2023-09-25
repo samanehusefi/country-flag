f26e920e82fb24697b2626cb81b3dc9844cae7be
s.yousefi <s.yousefi@DESKTOP-D8H9TO2>
2023-09-25 09:50:48 +0330

    add play image

--- pages/pomodoro.vue
+++ pages/pomodoro.vue
@@ -4,8 +4,12 @@
             <div
                 :class="['bg-slate-200 h-screen  content-center flex flex-wrap items-center justify-center', showScreen ? '-ml-[25%] basis-1/2' : 'basis-1/3']">
                 <div class="h-[350px] w-[350px] relative" @click="showForm()">
-                    <img src="../images/tomato-4-svgrepo-com.svg" alt="" class="svg-time" />
-
+                    <img src="../images/tomato-4-svgrepo-com.svg" alt="tomato" />
+                    <svg width="100px" height="100px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="absolute left-[35%] top-[45%]">
+                        <path
+                            d="M16.6582 9.28638C18.098 10.1862 18.8178 10.6361 19.0647 11.2122C19.2803 11.7152 19.2803 12.2847 19.0647 12.7878C18.8178 13.3638 18.098 13.8137 16.6582 14.7136L9.896 18.94C8.29805 19.9387 7.49907 20.4381 6.83973 20.385C6.26501 20.3388 5.73818 20.0469 5.3944 19.584C5 19.053 5 18.1108 5 16.2264V7.77357C5 5.88919 5 4.94701 5.3944 4.41598C5.73818 3.9531 6.26501 3.66111 6.83973 3.6149C7.49907 3.5619 8.29805 4.06126 9.896 5.05998L16.6582 9.28638Z"
+                            stroke="#fff" stroke-width="2" stroke-linejoin="round" />
+                    </svg>
                 </div>
                 <div class="basis-full content-center flex flex-wrap items-center justify-center">
                     <h2 class="basis-full text-center text-6xl" v-if="showTimer">
@@ -96,7 +100,7 @@ const start = () => {
     debugger;
     validation.value = true;
 
-    if (validationNameForm.value.showError===false && validationEmailForm.value.showError===false) {
+    if (validationNameForm.value.showError === false && validationEmailForm.value.showError === false) {
         showTimer.value = true;
         palyform.value = true;
         showforms.value = false;
@@ -156,9 +160,3 @@ const clearValidation = () => {
 
 
 
-<style>
-.svg-time {
-    position: absolute;
-    content: "../images/Play.svg";
-}
-</style>
