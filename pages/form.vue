f3433f0b25c7397b5c0eb2b3c2191e1f625b46ad
s.yousefi <s.yousefi@DESKTOP-D8H9TO2>
2023-09-27 17:42:47 +0330

    form

---
+++ pages/form.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="conatiner">
+        <div class="flex flex-wrap basis-1/3 w-[700px] justify-center ">
+            <div class="container">
+                <div class="flex justify-center flex-wrap w-full">
+                    <input class="w-full m-3 p-3 border-2 border  border-green-300 rounded-lg" v-bind="firstNameModel"
+                        placeholder="FirstName">
+                    <span class="text-red-300">{{ errors.firstName }}</span>
+                    <input class="w-full m-3 p-3 border-2 border  border-green-300 rounded-lg" v-bind="lastNameModel"
+                        placeholder="LastName">
+                    <span class="text-red-300">{{ errors.lastName }}</span>
+                    <input class="w-full m-3 p-3 border-2 border  border-green-300 rounded-lg" v-bind="phoneModel"
+                        placeholder="PhoneNumber">
+                    <span class="text-red-300">{{ errors.phoneNumber }}</span>
+                    <input class="w-full m-3 p-3 border-2 border  border-green-300 rounded-lg" v-bind="emailModel"
+                        placeholder="EmailAddress" />
+                    <span class="text-red-300">{{ errors.emailSchema }}</span>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+
+import { useForm } from 'vee-validate'
+import * as yup from 'yup';
+const isForeign = ref(true)
+
+const schema = yup.object({
+    emailSchema:yup.string().required("Please Enter Email").email("Please Enter a Correct Email"),
+
+    // emailSchema: yup.string().test((value, context) => {
+    //     if (isForeign.value) {
+    //         const customCondition = value
+    //         console.log("log==>", value, context)
+    //         return customCondition || context.createError("Please Enter a Correct Email")
+    //     }
+    //     return true
+
+    // }),
+
+    firstName: yup.string().min(2).max(100).required(),
+    lastName: yup.string().min(10).max(256).required(),
+    phoneNumber: yup.string().required().test((value, context) => {
+        const customPhoneCondition = valid(value)
+        return customPhoneCondition || context.createError({ message: "Please Enter a Correct Phone Number" })
+    })
+
+});
+
+
+
+const phoneNumberValidation = /^((\+98|0)?9\d{9})$/;
+
+
+const valid = (phone) => {
+ console.log("phoneNumber==>",phone)
+    return phone.length && phone.test(phoneNumberValidation)
+ 
+    
+}
+
+const { defineInputBinds, errors } = useForm({
+    validationSchema: schema
+})
+
+const emailModel = defineInputBinds('emailSchema')
+const firstNameModel = defineInputBinds('firstName')
+const lastNameModel = defineInputBinds('lastName')
+const phoneModel = defineInputBinds('phoneNumber')
+</script>
